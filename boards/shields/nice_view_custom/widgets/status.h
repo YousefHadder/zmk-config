a3bafa09dd6d2fcfd372c313d3b4d0d081a6a21f
yousefhadder <yousefhadder@github.com>
2025-08-10 22:11:11 -0500

    add nice!view code

---
+++ boards/shields/nice_view_custom/widgets/status.h
@@ -0,0 +1,26 @@
+/*
+ *
+ * Copyright (c) 2023 The ZMK Contributors
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#pragma once
+
+#include <lvgl.h>
+#include <zephyr/kernel.h>
+#include "util.h"
+
+struct zmk_widget_status {
+    sys_snode_t node;
+    lv_obj_t *obj;
+    lv_color_t cbuf[CANVAS_SIZE * CANVAS_SIZE];
+    lv_color_t cbuf2[CANVAS_SIZE * CANVAS_SIZE];
+    lv_color_t cbuf3[CANVAS_SIZE * CANVAS_SIZE];
+    struct status_state state;
+    uint8_t current_image_index;
+    struct k_work_delayable image_rotation_work;
+};
+
+int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent);
+lv_obj_t *zmk_widget_status_obj(struct zmk_widget_status *widget);
